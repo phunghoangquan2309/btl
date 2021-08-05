5fe0f65b07f7b78766fad0f64f5ba04541b21dc4
trongtai37 <dongthoigian37@gmail.com>
2021-08-05 11:04:37 +0700

    Update Todo Table

--- src/TodoWebapp/Home.js
+++ src/TodoWebapp/Home.js
@@ -1,79 +1,73 @@
-import { React, useState,useEffect } from 'react';
-import { useHistory } from "react-router-dom";
-import { Layout, Menu, Breadcrumb, Table, Button, Input, Tooltip } from 'antd';
-import TableTodo from './TableTodo';
+import { Button, Input, Layout } from 'antd';
+import { React, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import callAPI from '../ApiCall/ApiCaller';
 import { useAuth } from '../Context/AuthContext';
+import TableTodo from './TableTodo';
 const { Header, Content, Footer } = Layout;
 const { Search } = Input;
-const data = [
-  {
-    id: '5',
-    date: '23/09/2000',
-    time: '25h',
-    username:'Quan',
-    name: 'A',
-    status: 1,
-  },
-  {
-    id: '6',
-    date: '23/09/2000',
-    time: '24h',
-    name: 'B',
-    status: 0,
-  },
-  {
-    id: '3',
-    date: '23/09/2000',
-    time: '24h',
-    name: 'John Brown342',
-    status: 1,
-  },
-  {
-    id: '4',
-    date: '23/09/2000',
-    time: '24h',
-    name: 'John Brown342',
-    status: 0,
-  }
-
-];
 
 function Home() {
-  const [todolist, setTodoList] = useState([])
+  const [todoList, setTodoList] = useState([]);
   const { user } = useAuth();
   const history = useHistory();
-  
+
   function clickAdd() {
-    history.push("/home/addtodo")
+    history.push('/home/addtodo');
   }
+
   function onSearch(value) {
-    let endpoin = `search?username=${user.username}&name=${value}`;
-    callAPI(endpoin, "GET", null)
-    .then(res => {
-      for (var i = 0; i < res.data.length; i++) {
-        res.data[i].key = res.data[i].id;
-        delete res.data[i].id;
-      }
-      console.log(res.data)
-      setTodoList(res.data)
-    })
-    .catch(err => { alert("Loi tim kiem"); console.log(err) })
+    let endpoint = `search?username=${user.username}&name=${value}`;
+    callAPI(endpoint, 'GET', null)
+      .then((res) => {
+        for (var i = 0; i < res.data.length; i++) {
+          res.data[i].key = res.data[i].id;
+          delete res.data[i].id;
+        }
+        console.log(res.data);
+        setTodoList(res.data);
+      })
+      .catch((error) => {
+        alert('Loi tim kiem');
+        console.log(error.response.data); // data
+        console.log(error.response.status); // status code
+        console.log(error.response.headers); // headers
+      });
   }
+
+  React.useEffect(() => {
+    let endpoint = `getlist?username=${user.username}`;
+    callAPI(endpoint, 'GET', null)
+      .then((res) => {
+        for (var i = 0; i < res.data.length; i++) {
+          res.data[i].key = res.data[i].id;
+          delete res.data[i].id;
+        }
+        setTodoList(res.data);
+      })
+      .catch((err) => {
+        alert('Loi');
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Layout className="layout">
       <Header>
         <div className="logo" />
       </Header>
       <Content style={{ padding: '50px 50px' }}>
-        <Button onClick={clickAdd} type="primary">Add</Button>
+        <Button onClick={clickAdd} type="primary">
+          Add
+        </Button>
         <Search placeholder="input name" onSearch={onSearch} enterButton />
-        <TableTodo todoList={todolist} />
+        <TableTodo todoList={todoList} setTodoList={setTodoList} />
       </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
     </Layout>
-  )
+  );
 }
 
-export default Home
-
+export default Home;
--- src/TodoWebapp/TableTodo.js
+++ src/TodoWebapp/TableTodo.js
@@ -1,14 +1,10 @@
-import { Table, Tag, Space } from 'antd';
-import React, { Component, useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Space, Table, Tag } from 'antd';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import callAPI from '../ApiCall/ApiCaller';
 import { useAuth } from '../Context/AuthContext';
 
-
-
-
-function TableTodo({ todoList }) {
-  const [todolist, setTodoList] = useState(todoList)
+function TableTodo({ todoList, setTodoList }) {
   const { user } = useAuth();
   const columns = [
     {
@@ -20,7 +16,7 @@ function TableTodo({ todoList }) {
       title: 'Time',
       dataIndex: 'time',
       key: 'time',
-      render: text => <td>{text}</td>,
+      render: (text) => <td>{text}</td>,
     },
     {
       title: 'Name',
@@ -33,13 +29,9 @@ function TableTodo({ todoList }) {
       dataIndex: 'status',
       render: (status) => {
         let color = status == 1 ? 'green' : 'volcano';
-        status = status == 1 ? 'Done' : 'Not Done'
-        return (
-          <Tag color={color} >
-            {status.toUpperCase()}
-          </Tag>
-        );
-      }
+        status = status == 1 ? 'Done' : 'Not Done';
+        return <Tag color={color}>{status.toUpperCase()}</Tag>;
+      },
     },
     {
       title: 'Action',
@@ -48,47 +40,46 @@ function TableTodo({ todoList }) {
         return (
           <Space size="middle">
             <Link to={`/home/del?key=${record.key}`}>Edit</Link>
-            <Link onClick={() => {
-              if (window.confirm('Bạn có chắc chắn muốn xóa ?')) {
-                let endpoin = `delete?id=${record.key}`
-                callAPI(endpoin, "DELETE", null)
-                  .then(res => {
-                    if (res.status === 200) {
-                      alert("Thanh cong")
-                    const udtodolist = Object.values(todolist).filter(function (obj) {
-                        return obj.key !== record.key;
-                      });
-                      setTodoList(udtodolist)
-                    }
-  
-                  })
-                  .catch(err => { alert("Error"); console.log(err) })
-              }
-            }}>Delete</Link>
+            <Link
+              onClick={() => {
+                if (window.confirm('Bạn có chắc chắn muốn xóa ?')) {
+                  let endpoin = `delete?id=${record.key}`;
+                  callAPI(endpoin, 'DELETE', null)
+                    .then((res) => {
+                      if (res.status === 200) {
+                        alert('Thanh cong');
+                        const filteredTodos = Object.values(todoList).filter(
+                          function (obj) {
+                            return obj.key !== record.key;
+                          },
+                        );
+                        setTodoList(filteredTodos);
+                      }
+                    })
+                    .catch((err) => {
+                      alert('Error');
+                      console.log(err);
+                    });
+                }
+              }}
+            >
+              Delete
+            </Link>
           </Space>
-        )
-      }
+        );
+      },
     },
   ];
-  useEffect(() => {
-    let endpoin = `getlist?username=${user.username}`;
-    callAPI(endpoin, "GET", null)
-      .then(res => {
-        for (var i = 0; i < res.data.length; i++) {
-          res.data[i].key = res.data[i].id;
-          delete res.data[i].id;
-        }
-        setTodoList(res.data)
-      })
-      .catch(err => { alert("Loi"); console.log(err) })
-  }, [])
+
   return (
     <div>
-      <Table columns={columns} dataSource={todolist} rowKey={record => record.id} />
+      <Table
+        columns={columns}
+        dataSource={todoList}
+        rowKey={(record) => record.id}
+      />
     </div>
-  )
+  );
 }
 
-export default TableTodo
-
-
+export default TableTodo;
